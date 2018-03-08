37875b8875a40430c6920efde83da182e3c24cc4
msleckman <msleckman@ucsb.edu>
2018-03-08 14:21:45 -0800

    slider works

--- dogapp/app.R
+++ dogapp/app.R
@@ -6,6 +6,7 @@ library(leaflet)
 library(dplyr)
 
 
+
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
   
@@ -13,10 +14,10 @@ ui <- dashboardPage(
   dashboardHeader(title = "Dog Parks in Isla Vista and Goleta, California"),
   dashboardSidebar(
     sidebarMenu(
-      
       menuItem("Park", tabName = "tab_1"),
       sliderInput("park_size", "Park Size (Acres):", 0,135, 15),
-      selectInput("Accessibility", "Accessibility:", choices = unique(dog_parks_updated$Accessibility)),
+      selectInput("Accessibility", "Accessibility:", c("Unpaved" = "u", "Paved" = "p", "Nonaccessible" = "n")), 
+                  # choices = unique(dog_parks_updated$Accessibility)), label=,
       radioButtons("onleash", "Leash Options", choices = unique(dog_parks_updated$onleash)), #radiobuttons is for radiobuttons
       radioButtons("run", "Dog Run", choices = unique(dog_parks_updated$run))
     )
@@ -40,16 +41,16 @@ ui <- dashboardPage(
                   ##Widgets
                   
                   DT::renderDT({
-                    datatable(table1) %>% 
+                    datatable(table1) %>%
                       formatStyle(
                         'park_size',
                         'accessibility',
                         'on_leash'
                       )
-                    
-                    
+
+
                   })
-                  
+
                 )
               )
       )
@@ -61,31 +62,34 @@ ui <- dashboardPage(
 server <- function(input, output) {
   
   output$mymap <- renderLeaflet({
+    
     sub_map<-dog_parks_updated %>% 
-    filter(onleash == input$onleash & run == input$run & Accessibility == input$Accessibility)
-    leaflet(sub_map) %>%  
+      filter(onleash == input$onleash & run == input$run & Accessibility == input$Accessibility & size_acre <= input$park_size)
+    
+      leaflet(sub_map)%>%
       addProviderTiles("OpenStreetMap")%>% 
       #addTiles(urlTemplate = "https://mts1.google.com/vt/lyrs=s&hl=en&src=app&x={x}&y={y}&z={z}&s=G",
       #         attribution = 'Google') %>% 
-      setView(lng = -119.8, lat = 34.4, zoom = 11) %>% 
+      #setView(lng = -119.8, lat = 34.4, zoom = 11) %>% 
       addMarkers(lat = sub_map$`latitude-decimal`, 
                  lng = sub_map$`longitude-decimal`,
                  #clusterOptions = markerClusterOptions(),
                  popup = as.character(dog_parks_updated$park_name))
   })
   
-  df<- dog_parks_updated
-  df_subset<- reactive({
-    a<- subset(df, size_acre == input$park_size, onleash == input$onleash, run == input$run)
-    return(a)
-    
-  })
-  
+  # df<- dog_parks_updated
+  # df_subset<- reactive({
+  #   a<- subset(df, size_acre == input$park_size, onleash == input$onleash, run == input$run)
+  #   return(a)
+  #   
+  # })
+  # 
   output$table1 = renderDT(
-    sub_table<-dog_parks_updated %>% 
-      filter(onleash == input$onleash & run == input$run & Accessibility == input$Accessibility)
+    sub_table<-dog_parks_updated %>%
+      filter(onleash == input$onleash & run == input$run & Accessibility == input$Accessibility & size_acre <= input$park_size) %>% 
+      rename
   )
-}
+ }
 
 
 
